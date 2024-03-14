bf1e14517b1adb83f041461bc1f3a34a9a25e731
AukawutDev <88315011+Aukawut@users.noreply.github.com>
2024-03-14 16:24:48 +0700

    first commit

--- api.php
+++ api.php
@@ -10,30 +10,18 @@ header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Disposition, Content-Type, Content-Length, Accept-Encoding,Authorization");
 header('Content-Type: application/json');
 require_once("./Controller/AuthController.php");
-require_once("./Controller/PartsController.php");
-require_once("./Controller/InspectionController.php");
-require_once("./Controller/ResultController.php");
 require_once("./Controller/UserController.php");
-require_once("./Controller/DashboardController.php");
-require_once("./Controller/LineNotifyController.php");
 
 // สร้าง Class API
 class API
 {
-    public $PartsController;
     public $UserController;
     public $AuthController;
-    public $InspectionController;
-    public $ResultController;
-    public $DashboardController;
     public function __construct()
     {
         $this->AuthController = new AuthController();
-        $this->PartsController = new PartsController();
-        $this->InspectionController = new InspectionController();
-        $this->ResultController = new ResultController();
         $this->UserController = new UserController();
-        $this->DashboardController = new DashboardController();
+
     }
 }
 
@@ -49,99 +37,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             switch ($req->router) {
                 case "allUsers":
                     $api->UserController->getUsers();
-                    break;
-                case "addUser":
-                    $api->UserController->addUsers($req);
-                    break;
-                case "updateUser":
-                    $api->UserController->updateUsers($req);
-                    break;
-                case "deleteUser":
-                    $api->UserController->deleteUser($req);
-                    break;
-                case "login":
-                    $api->AuthController->Login($req); //Method
-                    break;
-                case "auth":
-                    $api->AuthController->AuthToken($req); //Method
-                    break;
-                case "getAllParts":
-                    $api->PartsController->index(); //Method
-                    break;
-                case "getPartById":
-                    $api->PartsController->getPartById($req);
-                    break;
-                case "addPart":
-                    $api->PartsController->addPart($req);
-                    break;
-                case "getPathByBsthName":
-                    $api->PartsController->getPathByBsthName($req);
-                    break;
-                case "getDrawingImagePath":
-                    $api->PartsController->getDrawingImagePath($req);
-                    break;
-                case "updatePart":
-                    $api->PartsController->UpdatePart($req);
-                    break;
-                case "deletePart":
-                    $api->PartsController->deletePart($req);
-                    break;
-                case "inspection":
-                    $api->InspectionController->Inspection($req);
-                    break;
-                case "getRawDataByPartNo":
-                    $api->InspectionController->getRawDataByPartNo($req);
-                    break;
-                case "UpdateInspection":
-                    $api->InspectionController->UpdateInspection($req);
-                    break;
-                case "historyInspection":
-                    $api->InspectionController->historyInspection();
-                    break;
-                case "getInspectionNG":
-                    $api->InspectionController->getInspectionNG();
-                    break;
-                case "getInspectionFG":
-                    $api->InspectionController->getInspectionFG();
-                    break;
-                case "approveRecheck":
-                    $api->InspectionController->approveRecheck($req);
-                    break;
-                case "acceptNG":
-                    $api->InspectionController->acceptNG($req);
-                    break;
-                case "unApproveRecheck":
-                    $api->InspectionController->unApproveRecheck($req);
-                    break;
-                case "getInspectionPartByDatetime":
-                    $api->InspectionController->getInspectionPartByDatetime($req);
-                    break;
-                case "CheckInspectionNGStatus":
-                    $api->InspectionController->CheckInspectionNGStatus($req);
-                    break;
-                case "countMoldOfPsthPart":
-                    $api->InspectionController->countMoldOfPsthPart($req);
-                    break;
-                case "getListCavity":
-                    $api->InspectionController->getListCavity($req);
-                    break;
-                case "getStandardInspection":
-                    $api->InspectionController->index($req);
-                    break;
-                case "getStandardByPart":
-                    $api->InspectionController->getStandardByPart($req);
-                    break;
-                case "getAllResult":
-                    $api->ResultController->index();
-                    break;
-                case "getAmountInSystem":
-                    $api->DashboardController->index();
-                    break;
-                case "getDataXBarChart":
-                    $api->DashboardController->getDataXBarChart($req);
-                    break;
-                case "getXBarUCL":
-                    $api->DashboardController->getXBarUCL($req);
                     break;
                 default:
                     echo json_encode(["msg" => "Can't Request!"]);
